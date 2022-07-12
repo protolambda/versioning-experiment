4b39404c4f2ae88c816bb7f13cc03c69e0307409
protolambda <proto@protolambda.com>
2022-07-12 02:29:54 +0200

    now bar depends on foo

--- bar/go.mod
+++ bar/go.mod
@@ -1,3 +1,5 @@
 module github.com/protolambda/versioning-experiment/bar
 
 go 1.18
+
+require github.com/protolambda/versioning-experiment/foo v0.3.0
--- bar/hello.go
+++ bar/hello.go
@@ -1,5 +1,10 @@
 package bar
 
+import (
+	"fmt"
+	"github.com/protolambda/versioning-experiment/foo"
+)
+
 func HelloWorld() string {
-	return "hello bar non-canon change, merged with main branch change"
+	return fmt.Sprintf("hello bar non-canon change, merged with main branch change (wrapping foo: %q)", foo.HelloWorld())
 }
